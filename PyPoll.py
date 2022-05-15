5bda6e50a59ea87b61376fc62cac9719fc4670b2
cewarkentin <101836882+cewarkentin@users.noreply.github.com>
2022-05-15 14:57:35 -0500

    Reading CSV for project (3.4.4)

--- PyPoll.py
+++ PyPoll.py
@@ -10,19 +10,16 @@
 import csv
 import os
 
-# Assign a variable for the file to load and the path.
+# Assign a variable to load a file from a path.
 file_to_load = 'Resources\election_results (1).csv'
 
-# Create a filename variable to a direct or indirect path to the file.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
+# Assign a variable to save the file to a path.
+file_to_save = 'analysis\election_analysis.txt'
 
-# Using the with statement open the file as a text file.
-with open(file_to_save, "w") as txt_file:
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
 
-    # Write some data to the file.
-    txt_file.write("Hello World!\n")
-
-    # Write three counties to the file.
-    txt_file.write("Arapahoe, ")
-    txt_file.write("Denver, ")
-    txt_file.write("Jefferson")
+    # Read and print the header row.
+    headers = next(file_reader)
+    print(headers)
