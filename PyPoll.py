8267820bd5d9b9f2caa029b899dc61ea932680cc
cewarkentin <101836882+cewarkentin@users.noreply.github.com>
2022-05-15 14:47:42 -0500

    Writing text files practice

--- PyPoll.py
+++ PyPoll.py
@@ -8,4 +8,21 @@
 
 # import module for pulling data from external CSV files and perform operations on them
 import csv
-dir(csv)
+import os
+
+# Assign a variable for the file to load and the path.
+file_to_load = 'Resources\election_results (1).csv'
+
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Using the with statement open the file as a text file.
+with open(file_to_save, "w") as txt_file:
+
+    # Write some data to the file.
+    txt_file.write("Hello World!\n")
+
+    # Write three counties to the file.
+    txt_file.write("Arapahoe, ")
+    txt_file.write("Denver, ")
+    txt_file.write("Jefferson")
