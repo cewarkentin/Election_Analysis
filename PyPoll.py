cf96ef6867c61440ffd13380e009f042def0a006
cewarkentin <101836882+cewarkentin@users.noreply.github.com>
2022-05-14 21:43:46 -0500

    Starting code

---
+++ PyPoll.py
@@ -0,0 +1,11 @@
+# Goals:
+    # The total number of votes cast
+    # A complete list of candidates who received votes
+    # The percentage of votes each candidate won
+    # The total number of votes each candidate won
+    # The winner of the election based on popular vote
+
+
+# import module for pulling data from external CSV files and perform operations on them
+import csv
+dir(csv)
