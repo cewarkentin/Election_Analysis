b588d3bc1c56119c7073f0cce5d23dbadef1dd29
cewarkentin <101836882+cewarkentin@users.noreply.github.com>
2022-05-16 23:44:38 -0500

    county counts - percentage

--- county_counts.py
+++ county_counts.py
@@ -1,7 +1,7 @@
 # Goals:
-    # A. The number of votes for each county precinct
-    # B. The percentage of votes for each county precinct
-    # C. Which county had the largest number of votes?
+    # X. The number of votes for each county precinct
+    # Y. The percentage of votes for each county precinct
+    # Z. Which county had the largest number of votes?
 
 
 # import module for pulling data from external CSV files and perform operations on them
@@ -14,13 +14,13 @@ file_to_load = 'Resources\election_results (1).csv'
 # Assign a variable to save the file to a path.
 file_to_save = 'analysis\election_analysis.txt'
 
-# Initialize a total vote counter.
+# Y. Initialize a total vote counter.
 total_votes = 0
 
-# A. County options
+# X. County options
 county_options = []
 
-# A. Empty dictionary for county votes
+# X. Empty dictionary for county votes
 county_votes = {}
 
 # Open the election results and read the file
@@ -32,29 +32,42 @@ with open(file_to_load) as election_data:
 
     # Print each row in the CSV file.
     for row in file_reader:
-        # Add to the total vote count.
+        # Y. Add to the total vote count.
         total_votes += 1
 
-        # A. Print the county name from each row.
+        # X. Print the county name from each row.
         county_name = row[1]
 
-        # If the county name does not match any existing county ...
+        # X. If the county name does not match any existing county ...
         if county_name not in county_options:
 
-            # Add it to the list of counties
+            # X. Add it to the list of counties
             county_options.append(county_name)
 
-            # A. Begin tracking that county's vote count
+            # X. Begin tracking that county's vote count
             county_votes[county_name] = 0
 
-        # A. Add a vote to that county's count
+        # X. Add a vote to that county's count
         county_votes[county_name] += 1
 
-# Print the total votes.
+# Y. Print the total votes.
 print(total_votes)
 
-# Print the candidate list.
+# X. Print the county list.
 print(county_options)
 
-# Print the county vote dictionary
+# X. Print the county vote dictionary
 print(county_votes)
+
+# Y. Determine the percentage of votes for each county by looping through the counts
+# Y. Iterate through the county list
+for county_name in county_votes:
+
+    # Y. Retrieve vote counts for a county
+    cvotes = county_votes[county_name]
+
+    # Y. Calculate percentage of votes
+    cvote_percentage = float(cvotes) / float(total_votes) * 100
+
+    # Y. Print the county name and percentage of votes.
+    print(f"{county_name} contained {cvote_percentage:.1f}% of the total voters.")
